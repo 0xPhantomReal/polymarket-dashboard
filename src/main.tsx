c0083ee5e1bf9e97da2672a53c9385c124842f8e
0xPhantomReal <0xphantomreal@gmail.com>
2026-02-20 14:57:28 -0500

    Create entry point file src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+// Entry point of the polymarket-dashboard application
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
