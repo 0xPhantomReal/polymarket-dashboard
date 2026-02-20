9b5b323590500b324305e7f1ab318a3bb1903ed2
0xPhantomReal <0xphantomreal@gmail.com>
2026-02-20 14:56:32 -0500

    Create ActivityFeed.tsx for recent trades

---
+++ src/components/ActivityFeed.tsx
@@ -0,0 +1,14 @@
+// ActivityFeed.tsx
+
+import React from 'react';
+
+const ActivityFeed: React.FC = () => {
+    return (
+        <div>
+            <h2>Recent Trades</h2>
+            {/* Render recent trades here */}
+        </div>
+    );
+};
+
+export default ActivityFeed;
