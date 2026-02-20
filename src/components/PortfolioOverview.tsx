13489b1f137d2260b3d58710533567c530d6efa3
0xPhantomReal <0xphantomreal@gmail.com>
2026-02-20 14:56:47 -0500

    Create PortfolioOverview component for portfolio summary

---
+++ src/components/PortfolioOverview.tsx
@@ -0,0 +1,13 @@
+// PortfolioOverview.tsx
+import React from 'react';
+
+const PortfolioOverview: React.FC = () => {
+    return (
+        <div>
+            <h1>Portfolio Summary</h1>
+            {/* Add portfolio summary details here */}
+        </div>
+    );
+};
+
+export default PortfolioOverview;
