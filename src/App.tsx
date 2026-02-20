b703420e6d628c2ab414aef2f5d39e62dab20625
0xPhantomReal <0xphantomreal@gmail.com>
2026-02-20 14:57:20 -0500

    Create main dashboard component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Main Dashboard</h1>
+            {/* Add your dashboard components here */}
+        </div>
+    );
+};
+
+export default App;
